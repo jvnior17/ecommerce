371fa12b9b465e5c2865c3d13496af91a8e7d468
jvnior17 <paulj.amponsah@gmail.com>
2021-12-27 16:30:11 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import React, { Component }  from 'react';
+
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
